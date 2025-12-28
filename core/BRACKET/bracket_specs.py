6892399e519d21fb4b6c1d1dcfac300a8ffb20b8
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-28 15:43:51 +0900

    브래킷 모듈 리팩토링
    브래킷 프로토콜용 데이터클래스 추가

---
+++ core/BRACKET/bracket_specs.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+from utils.util import Direction
+
+
+@dataclass
+class BracketSpec:
+    """브래킷 프로토콜용 데이터클래스
+    Attributes:
+        element_type: I/O타입
+        bracket_type: # inner / outer
+        install_type: # OpG / Tn
+        gauge: 건식게이지
+        direction: 방향
+        name: 브래킷 풀네임
+        index: 브래킷 인덱스
+    """
+    element_type: str      # I / O
+    bracket_type: str      # inner / outer
+    install_type: str      # OpG / Tn
+    gauge: float
+    direction: Direction
+    name: str
+    index: int
