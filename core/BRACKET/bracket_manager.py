700cad3b2d313ef13eeacfbcd2f7566e94127a4f
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-28 15:44:55 +0900

    브래킷 모듈 리팩토링
    기존 bracktes의 핵심 클래스 기능 단순화로 분리

---
+++ core/BRACKET/bracket_manager.py
@@ -0,0 +1,30 @@
+
+from core.BRACKET.bracket_dictionary import Dictionaryofbracket
+from core.BRACKET.bracket_policy import BracketPolicy
+from core.base_manager import BaseManager
+from utils.logger import logger
+
+class BracketManager(BaseManager):
+    """
+     가동브래킷매니저 BaseManager상속
+    """
+
+    def __init__(self, dataloader, poledata):
+        super().__init__(dataloader, poledata)
+        logger.debug(f'BracketManager 초기화 완료')
+
+    def run(self):
+        self.create_bracket()
+
+    def create_bracket(self):
+        policy = BracketPolicy()
+        for i, pole in enumerate(self.poledata.poles):
+            try:
+                spec = policy.decide(
+                    index=i,
+                    pole=pole,
+                    dataloader=self.loader
+                )
+                pole.apply_bracket(spec)
+            except Exception:
+                logger.exception(f"Bracket 생성 실패 (index={i})")
