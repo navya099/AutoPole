a2a83f5b9374f7c1db048cab4017014eb48489f6
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-28 14:03:51 +0900

    pole_position_manager리팩토링
    PoleFileSource 신설

---
+++ core/POLE/pole_file_source.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+from core.POLE.polepositionset import PolePositionSet
+from fileio.fileloader import TxTFileHandler
+
+class PoleFileSource:
+    def load(self) -> PolePositionSet:
+        handler = TxTFileHandler()
+        handler.select_file(
+            "미리 정의된 전주 파일 선택",
+            [("txt files", "*.txt"), ("All files", "*.*")]
+        )
+
+        df = pd.read_csv(
+            handler.get_filepath(),
+            sep=",",
+            header=0,
+            names=['측점', '전주번호', '타입', '에어조인트']
+        )
+
+        return PolePositionSet(
+            positions=df['측점'].tolist(),
+            post_numbers=list(zip(df['측점'], df['전주번호'])),
+            post_types=list(zip(df['측점'], df['타입'])),
+            airjoints=[
+                (row['측점'], row['에어조인트'])
+                for _, row in df.iterrows()
+                if row['에어조인트'] != '일반개소'
+            ]
+        )
