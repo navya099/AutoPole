7a11d093a5186ef6c67a8d668c0ba44cebff3488
navya099 <qltmxkr@gmail.com>
2025-12-29 17:47:59 +0900

    Update base_manager.py
    BaseManager의 시그니쳐 오류 수정

--- core/base_manager.py
+++ core/base_manager.py
@@ -5,12 +5,8 @@ from fileio.dataloader import DataLoader
 
 class BaseManager:
     """MastManager와 BracketManager의 공통 기능을 관리하는 부모 클래스
-
     Attributes:
-        poledata (Optional[PoleDATAManager]): 전주 데이터
         loader (DataLoader): 데이터로더 객체
     """
-
-    def __init__(self, dataloader: DataLoader, poledata = None):
-        self.poledata =  poledata  # ✅ PoleDATAManager.poledata 인스턴스를 가져옴
+    def __init__(self, dataloader: DataLoader):
         self.loader = dataloader  # ✅ DataLoader 인스턴스를 가져옴
