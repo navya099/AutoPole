076399307a41d14b737f02565cacad620347e341
navya099 <qltmxkr@gmail.com>
2025-12-29 10:46:29 +0900

    Update mast_manager.py
    다른 종류와 동일한 구조로 수정

--- core/MAST/mast_manager.py
+++ core/MAST/mast_manager.py
@@ -1,32 +1,16 @@
+from core.MAST.mast_policy import MastPolicy
 from core.base_manager import BaseManager
-
+from utils.logger import logger
 
 class MastManager(BaseManager):
-    """전주(Mast) 데이터를 설정하는 클래스"""
-
-    def __init__(self, dataloader, poledata):
-        super().__init__(dataloader, poledata)
 
     def run(self):
-        self.create_mast()
+        policy = MastPolicy()
+        speed = self.loader.databudle.designspeed
 
-    def create_mast(self):
-        data = self.poledata
-        for i in range(len(data.poles)):
+        for i, pole in enumerate(self.poledata.poles):
             try:
-                current_structure = data.poles[i].current_structure
-                mast_index, mast_name = get_mast_type(self.loader.databudle.designspeed, current_structure)
-                data.poles[i].mast.name = mast_name
-                data.poles[i].mast.index = mast_index
-                data.poles[i].mast.direction = data.poles[i].Brackets[0].direction
-
-            except Exception as ex:
-                error_message = (
-                    f"예외 발생 in create_mast!\n"
-                    f"인덱스: {i}\n"
-                    f"예외 종류: {type(ex).__name__}\n"
-                    f"예외 메시지: {ex}\n"
-                    f"전체 트레이스백:\n{traceback.format_exc()}"
-                )
-                logger.error(error_message)
-                continue
+                spec = policy.decide(pole, speed)
+                pole.apply_mast(spec)
+            except Exception:
+                logger.exception(f"Mast 생성 실패 (index={i}, 측점={pole.pos})")
