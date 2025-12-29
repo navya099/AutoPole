b5671a965c13bd95ed8343651bacd7917ff64a3a
navya099 <qltmxkr@gmail.com>
2025-12-29 17:51:03 +0900

    리팩토링: 추상레이어와 오브젝트 레이어 분리
    추상레이어 계층인 PoleRefData 생성

---
+++ core/POLE/pole_refdata.py
@@ -0,0 +1,37 @@
+from point3d import Point3d
+
+class PoleRefData:
+    """
+    전주 '기준 위치' 데이터 (불변, 추상)
+    Attributes:
+        pos: 측점
+        span: 다음 전주경간
+        curve_type: 곡선
+        radius: 곡선반경
+        cant: 캔트
+        pitch: 구배
+        structure_type: 구조물
+        center_coord: 선형 좌표 3d
+        azimuth: 선형 진행 방위각
+    """
+    def __init__(self):
+        self.pos: float = 0.0
+        self.span: float = 0.0
+
+        # 선형
+        self.curve_type: str = ''
+        self.radius: float = 0.0
+        self.cant: float = 0.0
+        self.pitch: float = 0.0
+
+        # 구조물
+        self.structure_type: str = ''
+
+        # 공간 정보
+        self.center_coord: Point3d = Point3d(0, 0, 0)
+        self.azimuth: float = 0.0
+
+    @property
+    def is_last(self) -> bool:
+        return self.span == 0
+
