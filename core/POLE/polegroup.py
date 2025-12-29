0b572fbde8f54dce94c0a9348e7b27d4ba03f565
navya099 <qltmxkr@gmail.com>
2025-12-29 17:52:45 +0900

    Update polegroup.py
    리스트에서 딕셔너리로 변경

--- core/POLE/polegroup.py
+++ core/POLE/polegroup.py
@@ -1,10 +1,21 @@
-from core.POLE.poledata import PoleDATA
+from core.POLE.poledata import PolePlaceDATA
 
 
 class PoleGroup:
     def __init__(self, pos: int):
         self.pos = pos
-        self.poles: list[PoleDATA] = []
+        self.poles: dict[int, PolePlaceDATA] = {}
 
-    def add_pole(self, pole: PoleDATA):
-        self.poles.append(pole)
+    def get(self, trackidx: int) -> PolePlaceDATA | None:
+        return self.poles.get(trackidx)
+
+    def has_track(self, trackidx: int) -> bool:
+        return trackidx in self.poles
+
+    def add_pole(self, trackidx: int, pole: PolePlaceDATA):
+        if trackidx in self.poles:
+            raise ValueError(
+                f"Duplicate pole for track {trackidx} at pos {self.pos}"
+            )
+        self.poles[trackidx] = pole
+
