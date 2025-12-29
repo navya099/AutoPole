c9944f135a94dafad4fcaa4b687b8c242489db6f
navya099 <qltmxkr@gmail.com>
2025-12-29 17:54:16 +0900

    Update mast_manager.py
    변경된 구조로 매니저 개선

--- core/MAST/mast_manager.py
+++ core/MAST/mast_manager.py
@@ -1,16 +1,43 @@
+from core.MAST.mast_builder import MastBuilder
 from core.MAST.mast_policy import MastPolicy
+from core.POLE.poledata import PolePlaceDATA
 from core.base_manager import BaseManager
+from utils.util import Direction
 from utils.logger import logger
 
 class MastManager(BaseManager):
+    def __init__(self, dataloader, polerefdatas):
+        super().__init__(dataloader)
+        self.polerefdatas = polerefdatas
+        self.poledata: list[PolePlaceDATA] = []
+
 
     def run(self):
+
         policy = MastPolicy()
         speed = self.loader.databudle.designspeed
+        track_count = self.loader.databudle.linecount
 
-        for i, pole in enumerate(self.poledata.poles):
-            try:
-                spec = policy.decide(pole, speed)
-                pole.apply_mast(spec)
-            except Exception:
-                logger.exception(f"Mast 생성 실패 (index={i}, 측점={pole.pos})")
+
+        base_direction = (
+            Direction.LEFT if self.loader.databudle.poledirection == -1
+            else Direction.RIGHT
+        )
+
+        builder = MastBuilder()
+
+        for ref in self.polerefdatas:
+            for track_idx in range(track_count):
+                try:
+                    pole = builder.build(
+                        ref=ref,
+                        track_idx=track_idx,
+                        base_direction=base_direction,
+                        policy=policy,
+                        speed=speed
+                    )
+                    self.poledata.append(pole)
+                except Exception:
+                    logger.exception(
+                        f"Mast 생성 실패 (pos={ref.pos}, track={track_idx})"
+                    )
