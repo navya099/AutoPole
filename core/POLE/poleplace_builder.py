ed9ba6708be038394547fc2992290052723c9f41
navya099 <qltmxkr@gmail.com>
2025-12-30 15:51:07 +0900

    Create poleplace_builder.py
    mast에서 분리함

---
+++ core/POLE/poleplace_builder.py
@@ -0,0 +1,27 @@
+from core.POLE.poledata import PolePlaceDATA
+from core.section.section_rules import SectionRule
+from utils.util import Direction
+import math
+
+class PolePlaceBuilder:
+    def build(self, ref, track_idx, base_direction) -> PolePlaceDATA:
+        pole = PolePlaceDATA()
+        pole.track_index = track_idx
+        pole.pos = ref.pos
+        pole.span = ref.span
+        pole.ref = ref
+        pole.direction = (
+            base_direction
+            if track_idx == 0
+            else Direction.opposite(base_direction)
+        )
+
+        pole.coord = ref.center_coord.copy()
+        pole.gauge = SectionRule.get_gauge(ref.structure_type)
+
+        isleft = (pole.direction == Direction.LEFT)
+
+        azimuth = ref.azimuth + math.pi / 2 if isleft else ref.azimuth - math.pi / 2
+        pole.coord.move(azimuth, pole.gauge)
+
+        return pole
