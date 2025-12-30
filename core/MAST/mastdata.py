8e551591fc05fa791737781d9090e80cbb5a18e6
navya099 <qltmxkr@gmail.com>
2025-12-30 15:54:25 +0900

    Update mastdata.py
     bve요소에서 설계 요소로 변경

--- core/MAST/mastdata.py
+++ core/MAST/mastdata.py
@@ -1,21 +1,15 @@
-from core.element import Element
+from core.bve_element import Element
+from core.design_element import DesignElement
 
 
-class MastDATA(Element):
+class MastDesign(DesignElement):
+    """전주 설계 정보
+        Attributes:
+            foundation_type: 기초 형식(사각기초,원형기초,특수기초)
+            mast_role: 전주의 역할(단독주, 빔주, 인출주)
+
     """
-     전주 요소  Element상속
-     Attributes:
-         height(float):  전주높이(m)
-         width(float): 전주폭(mm)
-         fundermentalindex(int):  전주기초 오브젝트 인덱스
-         fundermentaltype(str): 전주기초 타입
-         fundermentaldimension(float): 전주기초치수
-     """
-
     def __init__(self):
         super().__init__()
-        self.height: float = 0.0
-        self.width: float = 0.0
-        self.fundermentalindex: int = 0
-        self.fundermentaltype: str = ''
-        self.fundermentaldimension: float = 0.0
+        self.foundation_type: str = ''
+        self.mast_role: str = ''
