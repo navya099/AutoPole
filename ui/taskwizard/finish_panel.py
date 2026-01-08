f6876079180b72bde00a908438e794f9c525ac42
navya099 <qltmxkr@gmail.com>
2026-01-08 15:48:42 +0900

    완료 패널 분리
    코드 구조 일관성 유지

---
+++ ui/taskwizard/finish_panel.py
@@ -0,0 +1,8 @@
+from ui.taskwizard.steppanels import StepPanel
+import tkinter as tk
+from tkinter import ttk
+
+class FinishPanel(StepPanel):
+    def show(self):
+        tk.Label(self.master, text="마침", font=("Arial", 14)).pack(pady=10)
+        tk.Label(self.master, text="모든 작업이 끝났습니다!", font=("Arial", 12)).pack(pady=10)
