4b0c20cb990b533d6b8325fbb98bb700a55d7762
navya099 <qltmxkr@gmail.com>
2026-01-09 16:31:23 +0900

    PlacementBuildeWindow 구현
    버튼 2개와 체크박스 1개 임시구현체
    run 연결

--- ui/placement_build_ui/placement_builde_windows.py
+++ ui/placement_build_ui/placement_builde_windows.py
@@ -1,6 +1,8 @@
 import tkinter as tk
-from tkinter import ttk, filedialog
-from tkinter import messagebox
+from tkinter import ttk
+
+from placement.place_batch_manager import PlaceBatchManager
+
 
 class PlacementBuildeWindow(tk.Toplevel):
     def __init__(self, master, design_context):
@@ -10,5 +12,27 @@ class PlacementBuildeWindow(tk.Toplevel):
         self.geometry("300x300")
         self.resizable(True, True)
 
+        container = ttk.Frame(self, padding=10)
+        container.pack(fill="both", expand=True)
+        # 버튼 영역
+        # -----------------------------
+        btn_frame = ttk.Frame(self, padding=10)
+        btn_frame.pack(fill="x")
+
+        self.commitbutton = ttk.Button(btn_frame, text="생성", command=self.run)
+        self.commitbutton.pack(side="left", padx=5)
+
+        self.exit = ttk.Button(btn_frame, text="닫기", command=self.destroy)
+        self.exit.pack(side="left", padx=5)
+
+        #체크박스
+        chk_frame = ttk.Frame(self, padding=10)
+        chk_frame.pack(fill="x")
+
+        self.range_chk_var = tk.IntVar()
+        self.range_chk_box = ttk.Checkbutton(chk_frame, variable=self.range_chk_var)
+        self.range_chk_box.pack(side="left", padx=5)
+
     def run(self):
-        pass
+        ir = PlaceBatchManager(self.design_context)
+        ir.run()
